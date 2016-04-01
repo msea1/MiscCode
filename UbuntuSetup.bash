2e767e9130bb5d10de72bedca4c78d95b7c8e120
msea1 <carruthm@gmail.com>
2016-03-31 22:15:21 -0700

    st3 packages

--- UbuntuSetup.bash
+++ UbuntuSetup.bash
@@ -10,4 +10,4 @@ apt-get install -y sublime-text-installer
 
 wget -O ~/.config/sublime-text-3/Installed\ Packages/Package\ Control.sublime-package https://sublime.wbond.net/Package%20Control.sublime-package --no-check-certificate
 
-
+printf "{\n\t\"installed_packages\":[\n\t\t\"AutoFileName\",\"BracketHighlighter\",\"ExportHtml\",\"GenerateUUID\",\"HexViewer\",\"Neon Color Scheme\",\"PackageResourceViewer\",\"PlistJsonConverter\",\"Python Flake8 Lint\",\"Python Improved\",\"SideBarEnhancements\",\"SideBarGit\",\"SublimeCodeIntel\",\"SublimeREPL\",\"Tag\",\"Terminal\"]}" > ~/.config/sublime-text-3/Packages/User/Package\ Control.sublime-settings
