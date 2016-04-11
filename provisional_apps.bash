434cde7f32087c9a747695553697958da9fa088b
Matthew Carruth <carruthm@gmail.com>
2016-04-11 09:10:46 -0700

    provy apps

---
+++ provisional_apps.bash
@@ -0,0 +1,11 @@
+# lightzone
+sudo add-apt-repository -y ppa:otto-kesselgulasch/lightzone
+sudo apt-get install lightzone
+
+# darkTable
+sudo add-apt-repository -y ppa:pmjdebruijn/darktable-release
+sudo apt-get install darktable
+
+# pycharm
+sudo add-apt-repository ppa:mystic-mirage/pycharm
+sudo apt-get install pycharm
