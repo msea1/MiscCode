9651e3a240b37edd04a865fa56d23a497f3ef9d3
Matthew Carruth <carruthm@gmail.com>
2016-11-23 14:42:09 -0800

    ferns

--- Imaging/Anki/imagemagick.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-mogrify -format anki *.jpg
-mogrify -format anki *.jpeg
-mogrify -format anki *.png
-
-convert '*.anki' -resize 125x125^ -gravity center -extent 125x125 -set filename:fname '%t_anki' +adjoin '%[filename:fname].jpeg'
-
-rm *.anki
