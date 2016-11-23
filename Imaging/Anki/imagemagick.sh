a50120d9c7d48b93c939434ecf50030c160253d2
Matthew Carruth <carruthm@gmail.com>
2016-11-23 13:41:11 -0800

    finished

--- Imaging/Anki/imagemagick.sh
+++ Imaging/Anki/imagemagick.sh
@@ -1,14 +1,9 @@
 #!/bin/sh
 
-for f in *.jpg; do
-mv -- "$f" "$f.jpeg"
-done
+mogrify -format anki *.jpg
+mogrify -format anki *.jpeg
+mogrify -format anki *.png
 
-for f in *.png; do
-mv -- "$f" "$f.jpeg"
-done
+convert '*.anki' -resize 125x125^ -gravity center -extent 125x125 -set filename:fname '%t_anki' +adjoin '%[filename:fname].jpeg'
 
-convert '*.jpeg' -resize 125x125^ -gravity center -extent 125x125 resize%03d.jpeg
-
-#stop affixing of new extension
-#have new file_name capture original file_name
+rm *.anki
