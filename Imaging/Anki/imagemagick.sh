b8dc817e54d1374e698d857cff9e022e2b1db1b8
Matthew Carruth <carruthm@gmail.com>
2016-11-23 12:10:51 -0800

    stuff

--- Imaging/Anki/imagemagick.sh
+++ Imaging/Anki/imagemagick.sh
@@ -1,1 +1,14 @@
-convert "*.jpg[200x]" resize%03d.jpg
+#!/bin/sh
+
+for f in *.jpg; do
+mv -- "$f" "$f.jpeg"
+done
+
+for f in *.png; do
+mv -- "$f" "$f.jpeg"
+done
+
+convert '*.jpeg' -resize 125x125^ -gravity center -extent 125x125 resize%03d.jpeg
+
+#stop affixing of new extension
+#have new file_name capture original file_name
