dfda5f443e6ff34ff57b9e9e11bd501c8d8d749d
Matthew Carruth <mcarruth@blacksky.com>
2018-06-27 12:15:14 -0700

    mothra

--- Linux/mothra.sh
+++ Linux/mothra.sh
@@ -29,6 +29,7 @@ pause "Get TOMLs"
 cd ~./Code
 git clone git@git.spaceflight.com:block-2/gemini-mothra-tomls.git
 
+
 pause "Install TOMLs"
 cd gemini-mothra-tomls/
 cd command-toml/
@@ -37,12 +38,53 @@ sudo python3 setup.py install
 cd ../sap-toml/
 sudo python3 setup.py install
 
+
 pause "Test TOMLs. These may need to be run with sudo, or chown $USER first"
 python3 -m unittest
 cd ../command-toml/
 python3 -m unittest
 cd ../telemetry-toml/
 python3 -m unittest
+cd ../
+./pants test ::
 
+
+pause "Begin FSW installation"
 pause "Get Mothra code"
 cd ~./Code
+git clone git@git.spaceflight.com:block-2/mothra.git
+
+
+pause "Setup Curvesat"
+sudo apt install -y gcc make ruby-dev rubygems libsystemd-dev autoconf libconfuse-dev \
+    libdbus-1-dev libdbus-glib-1-dev libssl-dev python3-dev python3-pip rpm sharutils
+pip3 install virtualenv dbus-python
+sudo gem install fpm
+cd ./mothra/curvesat/
+make -j
+cd ../output/curvesat/
+
+
+pause "Install Curvesat, the actual name here depends on the current version of the package"
+sudo bash install-curvesat-1.105.sh 
+
+
+pause "Setup Mothra"
+cd ~/Code/mothra
+git submodule sync
+make sp0_defconfig
+cd output/sp0
+pause "Time to make Mothra. This will take ~30 minutes"
+make
+
+
+pause "Confirm Mothra"
+
+
+
+pause "Special Steps for Cmd&Seq"
+
+
+
+pause "Special Steps for sap-service"
+
