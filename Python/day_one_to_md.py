dc52c10aab7a123418a3aa89d2418a7899c57918
Matthew Carruth <mcarruth@blacksky.com>
2018-07-12 05:19:31 -0700

    journal

--- Python/day_one_to_md.py
+++ Python/day_one_to_md.py
@@ -1,18 +1,33 @@
-import arrow
 import argparse
 import json
+from os.path import basename, dirname, join, splitext
+
+import arrow
 import pytz
-
-from os.path import basename
-
 
 # GLOBALS #
 img_list = []
 
 
-def format_entry(time, geo, weather, star, text, image):
-    return f"**{time}**, _{weather}_ -- {text}\n" \
-           f"{':star2: ' if star}_{geo}_\n\n"
+def format_entry(time, geo, weather, star, subtitle, text, image):
+    entry = ''
+    if subtitle:
+        entry += f"**{subtitle}**:"
+    if time:
+        entry += f" _{time}_"
+    if weather:
+        entry += f" _{weather}_"
+    entry += "\n"
+    if image:
+        entry += image
+    if text:
+        entry += f"{text}\n"
+    if star:
+        entry += ':star:'
+    if geo:
+        entry += f"_{geo}_"
+    entry += "\n\n"
+    return entry
 
 
 def output_file(md_text, out_file):
@@ -37,7 +52,7 @@ def markdown_add_segue():
 def markdown_add_image(alt_text, url=None, rel_path=None):
     global img_list
     img_list.append(url if url else rel_path)
-    return f"![{alt_text}][{len(img_list)}]\n\n"
+    return f"\n![{alt_text}][{len(img_list)}]\n\n"
 
 
 def print_img_refs():
@@ -49,20 +64,20 @@ def print_img_refs():
 
 # TXT FORMAT #
 
-def parse_txt(journal_file, out_file):
+def parse_txt(_journal_file, _out_file):
     pass
 
 
 def parse_json(journal_file, out_file):
-    md = markdown_add_title(basename(out_file))
+    md = markdown_add_title(splitext(basename(out_file))[0].title())
     day = None
     with open(journal_file) as fin:
         journal = json.load(fin)
     entries = journal['entries']
     for entry in entries:
-        weather = format_weather_json(entry['weather'])
+        weather = format_weather_json(entry.get('weather', {}))
         starred = entry['starred']
-        geotag = format_location_json(entry['location'])
+        geotag = format_location_json(entry.get('location', {}))
 
         _day, time = format_date_json(entry)
         if _day != day:
@@ -71,25 +86,70 @@ def parse_json(journal_file, out_file):
         else:
             md += markdown_add_segue()
 
-        text = entry['text']
-        # TODO image
+        subtitle, text = format_text_json(entry['text'])
 
-        md += format_entry(time, geotag, weather, starred, text, image)
+        image = ""
+        if 'photos' in entry:
+            img = entry['photos'][0]  # currently only handles 1 image per entry
+            image = format_image_json(journal_file, img)
+
+        md += format_entry(time, geotag, weather, starred, subtitle, text, image)
+
+    md += f"\n\n{print_img_refs()}"
     output_file(md, out_file)
 
 
-def format_date_json(entry):  # TODO
-    creation_date = entry['creationDate']  # convert to readable
-    dt = arrow.get(creation_date)
+def format_text_json(text_str):
+    subtitle_i = text_str.find('\n')
+    if subtitle_i == -1:
+        return text_str, ''
+    subtitle = text_str[:subtitle_i]
+    subtitle_i += 1
+    while text_str[subtitle_i] == '\n':
+        subtitle_i += 1
 
-    time_zone = entry['timeZone']  # convert to shorthand
+    if 'dayone-moment' not in text_str:
+        return subtitle, text_str[subtitle_i:]
+
+    start_of_moment = text_str.find('![](dayone')
+    if start_of_moment == -1:
+        return text_str
+
+    prefix = text_str[subtitle_i:start_of_moment-2]
+    postfix_i = text_str[start_of_moment:].find(')')+start_of_moment+1
+    while postfix_i < len(text_str) and text_str[postfix_i] == '\n':
+        postfix_i += 1
+    postfix = text_str[postfix_i:]
+    return subtitle, prefix+postfix
+
+
+def format_image_json(file_dir, img_dict):
+    path = dirname(file_dir)
+    rel_path = join(path, 'photos', f'{img_dict["md5"]}.{img_dict["type"]}')
+    return markdown_add_image(img_dict['md5'], rel_path=rel_path)
+
+
+def format_date_json(entry):
+    creation_date = entry['creationDate']
+    adt = arrow.get(creation_date)
+
+    time_zone = entry['timeZone'].replace('\\', '')
     matches = [x for x in pytz.all_timezones_set if time_zone in x]
-    return 0, 0
+    dt = adt.datetime
+    if matches:
+        dt = adt.datetime.astimezone(pytz.timezone(time_zone))
+    day = dt.strftime('%a %d %b')
+    time = dt.strftime('%T') + f" {dt.tzname()}"
+    return day, time
 
 
 def format_weather_json(weather_dict):
-    return f'{weather_dict.get("conditionsDescription", "")} ' \
-           f'{weather_dict.get("windChillCelsius", "Unk")}°C'
+    desc = weather_dict.get("conditionsDescription", "")
+    temp = str(weather_dict.get("windChillCelsius", ""))
+    temp += '°C' if temp else ''
+    output = desc if desc else ''
+    output += f' {temp}' if temp and output else temp
+    return output
 
 
 def format_location_json(location_info):
@@ -99,11 +159,12 @@ def format_location_json(location_info):
         if location_info.get(x):
             geotag += f"{location_info[x]} "
     geotag = geotag.strip()
-    gps = f"{location_info.get('latitude', 0):.2f} " \
-          f"{location_info.get('longitude', 0):.2f}"
+    gps = f"{location_info.get('latitude', 0):.3f}, " \
+          f"{location_info.get('longitude', 0):.3f}"
+    gps = gps if gps != '0.000, 0.000' else ''
     if not geotag:
         return gps
-    return f"{geotag} {gps}"
+    return f"{geotag} {gps}".strip()
 
 
 if __name__ == "__main__":
@@ -111,8 +172,6 @@ if __name__ == "__main__":
     parser.add_argument('i', help='Input file, exported from Day One')
     parser.add_argument('o', help='Output file')
     args = parser.parse_args()
-    # print(f'{args.i}')
-    # print(f'{args.o}')
     if args.i.endswith('.txt'):
         parse_txt(args.i, args.o)
     elif args.i.endswith('.json'):
@@ -120,4 +179,3 @@ if __name__ == "__main__":
     else:
         print(f'Invalid input file, needs to be a txt or json, not {args.i}')
         exit(1)
-
--- Python/test_day_one.py
+++ Python/test_day_one.py
@@ -1,5 +1,5 @@
-import json
 import unittest
+
 import day_one_to_md as md
 
 
@@ -15,8 +15,8 @@ class TestMarkdownFormat(unittest.TestCase):
         self.assertEqual(md.markdown_add_segue(), '---\n\n')
 
     def test_add_image(self):
-        self.assertEqual(md.markdown_add_image('Hello', url='http'), '![Hello][1]\n\n')
-        self.assertEqual(md.markdown_add_image('Hello', rel_path='../photo'), '![Hello][2]\n\n')
+        self.assertEqual(md.markdown_add_image('Hello', url='http'), '\n![Hello][1]\n\n')
+        self.assertEqual(md.markdown_add_image('Hello', rel_path='../photo'), '\n![Hello][2]\n\n')
         self.assertEqual(len(md.img_list), 2)
         self.assertEqual(md.img_list[0], 'http')
         self.assertEqual(md.img_list[1], '../photo')
@@ -26,16 +26,133 @@ class TestMarkdownFormat(unittest.TestCase):
 class TestJsonFormat(unittest.TestCase):
     @classmethod
     def setUpClass(cls):
-        cls.input = {}
+        cls.input = {"entries" : [
+        {
+          "weather" : {
+            "sunsetDate" : "2018-05-19T00:02:21Z",
+            "temperatureCelsius" : 10.600000381469727,
+            "weatherServiceName" : "HAMweather",
+            "windBearing" : 110,
+            "sunriseDate" : "2018-05-18T09:18:43Z",
+            "conditionsDescription" : "Cloudy",
+            "pressureMB" : 1030,
+            "visibilityKM" : 16.093439102172852,
+            "relativeHumidity" : 54,
+            "windSpeedKPH" : 15,
+            "weatherCode" : "cloudy",
+            "windChillCelsius" : 11
+          },
+          "uuid" : "7ACE5A469B0A4E95A622C4342DDCFA0E",
+          "photos" : [
+            {
+              "cameraMake" : "Apple",
+              "fnumber" : "1.8",
+              "orderInEntry" : 0,
+              "lensMake" : "Apple",
+              "width" : 1575,
+              "cameraModel" : "iPhone 7",
+              "type" : "jpeg",
+              "identifier" : "771BFB4BA8E04650B70E0F0C3E07A559",
+              "date" : "2018-05-18T23:59:54Z",
+              "exposureBiasValue" : 0,
+              "location" : {
+                "region" : {
+                  "center" : {
+                    "longitude" : -71.018013000488281,
+                    "latitude" : 42.367694848285133
+                  },
+                  "identifier" : "<+42.36769485,-71.01801300> radius 141.69",
+                  "radius" : 141.68706608790438
+                },
+                "localityName" : "Boston",
+                "country" : "United States",
+                "timeZoneName" : "America\/New_York",
+                "administrativeArea" : "MA",
+                "longitude" : -71.018013000488281,
+                "placeName" : "Boston Logan International Airport",
+                "latitude" : 42.367694854736328
+              },
+              "height" : 2100,
+              "lensModel" : "iPhone 7 back camera 3.99mm f\/1.8",
+              "weather" : {
+                "sunsetDate" : "2018-05-19T00:02:21Z",
+                "temperatureCelsius" : 10.600000381469727,
+                "weatherServiceName" : "HAMweather",
+                "windBearing" : 110,
+                "sunriseDate" : "2018-05-18T09:18:43Z",
+                "conditionsDescription" : "Cloudy",
+                "pressureMB" : 1030,
+                "visibilityKM" : 16.093439102172852,
+                "relativeHumidity" : 54,
+                "windSpeedKPH" : 15,
+                "weatherCode" : "cloudy",
+                "windChillCelsius" : 11
+              },
+              "md5" : "2b9cabb0f836b94993374549f617b8f6",
+              "focalLength" : "3.99"
+            }
+          ],
+          "creationDeviceModel" : "iPhone9,3",
+          "creationDeviceType" : "iPhone",
+          "starred" : False,
+          "location" : {
+            "region" : {
+              "center" : {
+                "longitude" : -71.018013000488281,
+                "latitude" : 42.367694848285133
+              },
+              "identifier" : "<+42.36769485,-71.01801300> radius 141.69",
+              "radius" : 141.68706608790438
+            },
+            "localityName" : "Boston",
+            "country" : "United States",
+            "timeZoneName" : "America\/New_York",
+            "administrativeArea" : "MA",
+            "longitude" : -71.018013000488281,
+            "placeName" : "Boston Logan International Airport",
+            "latitude" : 42.367694854736328
+          },
+          "creationDate" : "2018-05-18T23:59:54Z",
+          "creationOSVersion" : "11.3.1",
+          "text" : "Airport snacking $9.50\n\n![](dayone-moment:\/\/771BFB4BA8E04650B70E0F0C3E07A559)\n\nShould not have done this. Overfed, predictably, on the flight. But I was bored.",
+          "timeZone" : "America\/New_York",
+          "creationOSName" : "iOS",
+          "creationDevice" : "iPhone",
+          "duration" : 0
+        },
+        ]}
 
     def test_weather_format(self):
-        pass
+        self.assertEqual("", md.format_weather_json(
+            {"conditionsDescription": "", "windChillCelsius": ""}))
+        self.assertEqual("Cloudy", md.format_weather_json(
+            {"conditionsDescription": "Cloudy", "windChillCelsius": ""}))
+        self.assertEqual("12°C", md.format_weather_json(
+            {"conditionsDescription": "", "windChillCelsius": "12"}))
 
     def test_location_format(self):
         pass
 
     def test_date_format(self):
-        pass
+        with self.assertRaises(KeyError):
+            md.format_date_json({})
+
+        self.assertEqual(("Sat 19 May", "22:05:44 +04"), md.format_date_json(
+            {"creationDate": "2018-05-19T18:05:44Z", "timeZone": "Asia\/Baku"}))
 
     def test_json_single_entry(self):
         pass
+
+    def test_json_text(self):
+        resp = md.format_text_json(
+            "A square-about\n\n![](dayone-moment:\/\/72D8DEF02D3B471CB9F7A32996F1B8B2)\n\nBless you Soviet Urban planning")
+        self.assertEqual(resp, ('A square-about', 'Bless you Soviet Urban planning'))
+        resp = md.format_text_json(
+            "Departure\nThe start of a long trip always seems overwhelming to me because I'm viewing the trip as a "
+            "whole rather than just what the day in front of me holds. Big surprise I'm not in the moment.\n\n"
+            "Fittingly it begins raining as we depart.")
+        self.assertEqual(resp, ("Departure", "The start of a long trip always seems overwhelming to me because "
+                               "I'm viewing the trip as a whole rather than just what the day in front of me holds. "
+                               "Big surprise I'm not in the moment.\n\nFittingly it begins raining as we depart."))
+        resp = md.format_text_json("Noravank. Wow")
+        self.assertEqual(resp, ("Noravank. Wow", ""))
