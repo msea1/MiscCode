55b2b5a344bacd6044f7f3c7936224782e9dad9f
Matthew Carruth <mcarruth@blacksky.com>
2019-03-08 11:39:32 -0800

    a

--- Linux/aliases.sh
+++ Linux/aliases.sh
@@ -28,6 +28,7 @@ alias files='xdg-open .'
 alias qemu='make && ./provision.sh -X -i images -q -S -c && cinderblock -i provision -Q host/usr/bin/qemu-system-ppc'
 alias root="sudo su -"
 alias sorry='sudo $(fc -ln -1)'
+alias update='sudo apt update && sudo apt -y upgrade'
 alias usb_f5='sudo usbmuxd -u -U usbmux'
 alias vauth='vault auth -method=ldap username=$USER'
 alias vssh='vault ssh -role otp_key_role'
