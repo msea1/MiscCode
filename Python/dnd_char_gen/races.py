9b708ea660982be713ae0ef5ee5c8f8e4d8f24f3
Matthew Carruth <mcarruth@blacksky.com>
2019-08-20 07:39:01 -0700

    randomly get a race

--- Python/dnd_char_gen/races.py
+++ Python/dnd_char_gen/races.py
@@ -15,30 +15,44 @@ needed for Race to start:
     then, add in other data
     and compare/weight to avoid all the dragonborns for example
 """
+DESIGNATED_NPC_RACES = ['changeling']
+
+
 class Race:
     def __init__(self, name, size, speed, ability, proficiency, trait, spellAbility="", modifier=""):
-        self.npc_only = 'npc' in name.lower()
+        self.npc_only = 'npc' in name.lower() or name.lower() in DESIGNATED_NPC_RACES
         self.name = name
         self.size = size
         self.speed = speed
         self.ability_str = ability
+        self.abilities = self.parse_abilities()
         self.proficiency = proficiency
+        self.parse_proficiency()
         self.trait = trait
         self.spell_ability = spellAbility
         self.modifier = modifier
 
     def __str__(self):
-        pass
+        return self.name
 
     def apply(self, character_obj):
         pass
 
-    def parse_abilities(self, ability_obj):
+    def parse_abilities(self):
+        abil = []
+        if not self.ability_str:
+            return []
         entries = self.ability_str.split(',')
         for entry in entries:
+            entry = entry.strip()
             ability_type = entry[:3].upper()
             ability_mod = int(entry[3:].strip())
-            ability_obj['stats'][ability_type] += ability_mod
+            # ability_obj['stats'][ability_type] += ability_mod
+            abil.append((ability_type, ability_mod))
+        return abil
+
+    def parse_proficiency(self):
+        self.proficiency = self.proficiency.split(',') if self.proficiency else ''
 
 
 class Universe:
@@ -50,7 +64,30 @@ class Universe:
         a = self.load_core()
         a.update(self.load_eberron())
         a.update(self.load_npc_race_data())
-        print(a)
+        a = self.match_race_to_already_existing(a)
+        self.data = a
+
+    @staticmethod
+    def match_race_to_already_existing(all_races):
+        attempt = {'Aasimar': [], 'Dragonborn': [], 'Dwarf': [], 'Elf': [], 'Genasi': [],
+                   'Gnome': [], 'Halfling': [], 'Shifter': [], 'Warforged': []}  # todo, automate this
+        to_process = []
+        for r, v in all_races.items():
+            if len(r.split()) == 1:
+                attempt[r] = [v]
+            else:
+                to_process.append((r, v))
+        for r, v in to_process:
+            words = r.split()
+            seen = False
+            for w in words:
+                if w in attempt:
+                    attempt[w].append(v)
+                    seen = True
+                    break
+            if not seen:
+                attempt[r] = [v]
+        return attempt
 
     def load_core(self):
         with open(join(self.data_folder, 'Core.xml')) as fin:
@@ -73,3 +110,29 @@ class Universe:
 
 uni = Universe()
 uni.load_data()
+
+from dnd_char_gen.utils import choose
+allow_npc = False
+
+
+def choose_race():
+    major_race = choose(list(uni.data), 1)[0]
+    if len(uni.data[major_race]) == 1:
+        return uni.data[major_race][0]
+    else:
+        subr = choose(uni.data[major_race], 1)[0]
+        i = uni.data[major_race].index(subr)
+        return uni.data[major_race][i]
+
+
+picks = 1
+pick = choose_race()
+if allow_npc:
+    print(pick)
+else:
+    while pick.npc_only:  # have to keep picking
+        pick = choose_race()
+        print(pick)
+        picks += 1
+    print(pick)
+print(picks)
