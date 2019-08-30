a912fb58dbc9c77a0e9494589762574b6b1ce47c
Matthew Carruth <mcarruth@blacksky.com>
2019-08-30 16:25:16 -0700

    multi image start

--- Python/misc_blacksky/multi_image_high_level_graph.py
+++ Python/misc_blacksky/multi_image_high_level_graph.py
@@ -3,13 +3,14 @@ import graphviz
 sqs_color = 'blue'
 fx_call = 'darkgreen'
 data_color = 'red'
+new_line = 'purple'
 
 service_shape = 'doubleoctagon'
 process = 'oval'
 data_store = 'cylinder'
 queue = 'note'
 
-G = graphviz.Digraph(format='png', filename='multi_image_overview.dg')
+G = graphviz.Digraph(format='png', filename='multi_overview.dg')
 
 # SATELLITE
 with G.subgraph(name='clustersat') as c:
@@ -18,6 +19,7 @@ with G.subgraph(name='clustersat') as c:
     c.node('metadata', label='downlink_metadata', shape=process)
 
 G.edge('image', 'xband_radio', color=data_color)
+G.edge('image', 'xband_radio', color=new_line)
 G.edge('metadata', 'xband_radio', color=data_color)
 
 
@@ -26,13 +28,13 @@ with G.subgraph(name='clustergroundstation') as c:
     c.attr(label='GROUND STATION')
     c.node('xband_radio', label='radio', shape=service_shape)
     c.node('rlm', label='sfx_file_monitor', shape=process)
-    c.node('sft', label='sft_file_transfer', shape=process)
+    c.node('sft_file', label='<sft_file_transfer<BR/><I>via sft-packet-router</I>>', shape=process)
     c.node('trebuchet', shape=service_shape)
 
 G.edge('rlm', 'xband_radio', dir='forward', arrowhead='inv', color=data_color)
-G.edge('sft', 'xband_radio', dir='forward', arrowhead='inv', color=data_color)
+G.edge('sft_file', 'xband_radio', dir='forward', arrowhead='inv', color=data_color)
 G.edge('rlm', 'trebuchet', color=fx_call)
-G.edge('sft', 'trebuchet', color=fx_call)
+G.edge('sft_file', 'trebuchet', color=fx_call)
 G.edge('trebuchet', 'files_s3', color=data_color)
 G.edge('trebuchet', 'env_files', color=sqs_color)
 G.edge('pdp', 'env_files', dir='forward', arrowhead='inv', color=sqs_color)
@@ -63,37 +65,40 @@ with G.subgraph(name='clustergemini') as gem:
         c.attr(label='OBSCURA', style='dashed')
         c.node('img_req', label='ImageRequest', shape=service_shape)
         c.node('workflow', label='WorkFlow', shape=service_shape)
+        c.node('multi', label='Multi Check', shape=service_shape, color=new_line)
         c.node('upload', label='Upload', shape=service_shape)
-        with c.subgraph(name='clusterimgreq') as cc:
-            cc.attr(label='ImageReqest')
-            cc.node('get_sfx', label='SFX file', shape=process)
-            cc.node('get_metadata', label='image metadata', shape=process)
-        with c.subgraph(name='clusterwf') as cc:
-            cc.attr(label='Workflow', style='dotted')
-            cc.node('run_wf', label='run workflow', shape=process)
-            cc.node('examine_task', label='<compare accuracy<BR/>vs task data>', shape=process)
-        # with c.subgraph(name='clusterupdload') as cc:
-        #     cc.attr(label='Upload', style='dotted')
+        c.node('get_sfx', label='SFX file', shape=process)
+        c.node('get_metadata', label='image metadata', shape=process)
+        c.node('get_task', label='task metadata', shape=process)
+        c.node('run_wf', label='run workflow', shape=process)
+        c.node('examine_task', label='<compare accuracy<BR/>vs task data>', shape=process)
+        c.node('post_process_check', label='<Part of multi<BR/>order and all<BR/>images processed?>', shape=process, color=new_line)
+        c.node('gather_multi_images', label='<Gather images<BR/>and create task<BR/>with stereo wf>', shape=process, color=new_line)
 
 G.edge('pdp', 'satmodel', arrowhead='inv', dir='forward', color=data_color)
 G.edge('pdp', 'cmd_gen', arrowhead='inv', dir='forward', color=data_color)
-G.edge('pdp', 'files_s3', color=data_color)
+G.edge('pdp', 'files_s3', color=data_color)  # decrypted image_metadata files
 G.edge('pdp', 'obscura_queue', color=sqs_color)
 
 G.edge('img_req', 'obscura_queue', arrowhead='inv', dir='forward', color=sqs_color)
 G.edge('img_req', 'get_sfx', arrowhead='inv', dir='forward', color=data_color)
+G.edge('img_req', 'get_task', arrowhead='inv', dir='forward', color=data_color)
 G.edge('get_sfx', 'files_s3', dir='both', color=data_color)
 G.edge('img_req', 'get_metadata', arrowhead='inv', dir='forward', color=data_color)
 G.edge('get_metadata', 'pdp', arrowhead='inv', dir='forward', color=data_color)
-G.edge('img_req', 'planner', arrowhead='inv', dir='forward', color=data_color)
+G.edge('get_task', 'planner', arrowhead='inv', dir='forward', color=data_color)
 
 G.edge('img_req', 'workflow', color=sqs_color)
 G.edge('workflow', 'run_wf', arrowhead='inv', dir='forward', color=data_color)
 G.edge('workflow', 'examine_task', color=fx_call)
 
-G.edge('workflow', 'upload', color=sqs_color)
+G.edge('workflow', 'multi', color=new_line)
+G.edge('multi', 'upload', color=new_line)
 G.edge('upload', 'final_product', color=data_color)
-G.edge('upload', 'satmodel', color=fx_call)
+G.edge('multi', 'post_process_check', color=new_line)
+G.edge('post_process_check', 'satmodel', label='No', color=new_line)
+G.edge('post_process_check', 'gather_multi_images', label=' Yes', color=new_line)
+G.edge('gather_multi_images', 'workflow',color=new_line)
 G.edge('upload', 'compliance', color=fx_call)
 
 
@@ -103,10 +108,12 @@ with G.subgraph(name='clusterlegend') as legend:
     legend.node('b', label='function', shape=process)
     legend.node('c', label='data store', shape=data_store)
     legend.node('d', label='SQS queue', shape=queue)
+    legend.node('e', label='N/A', shape=process)
 
     legend.edge('a', 'b', label=' foo()', color=fx_call)
-    legend.edge('b', 'c', label=' data', color=data_color)
+    legend.edge('b', 'c', label=' pull data', arrowhead='inv', color=data_color)
     legend.edge('c', 'd', label=' SQS', color=sqs_color)
+    legend.edge('d', 'e', label=' new', color=new_line)
 
 # G.attr(engine='neato')
 # G.attr(layout='fdp')
