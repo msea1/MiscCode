00b0bc09203c8d0d7b092904bc0115cea5a90f29
Matthew Carruth <mcarruth@blacksky.com>
2020-01-02 17:30:48 -0800

    moving to other repos

--- Imaging/Linux/gimp-matthew16.sh
+++
@@ -1,2 +0,0 @@
-cd ~/Images/
-gimp --verbose -d -f -i -b '(batch-matthew "*.[jJ][pP][gG]")' -b '(gimp-quit 0)'
--- Imaging/gimp.bat
+++
@@ -1,2 +0,0 @@
-cd c:\Batch
-gimp-2.6 --verbose -d -f -i -b "(batch-matthew \"*.jpg\")" -b "(gimp-quit 0)"
